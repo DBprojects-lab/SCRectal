ca3759028d96f2137caad2770e77b4cd60ee800f
wudeng <111028331+wudeng2022@users.noreply.github.com>
2023-08-31 10:35:05 +0800

    Add files via upload

---
+++ mapping.code.sh
@@ -0,0 +1,22 @@
+
+cd /boot2/bixm/02-GuanXu/
+
+/boot2/bixm/Software/cellRanger/cellranger-5.0.1/bin/cellranger count --id=R18072055 \
+--fastqs=/boot2/bixm/02-GuanXu/03_scRNAseq_20210628/Sample_R18072055_0_1_H7KJKDSXX \
+--sample=R18072055 \
+--transcriptome=/boot2/bixm/Software/cellRanger/refdata-gex-GRCh38-2020-A/
+
+/boot2/bixm/Software/cellRanger/cellranger-5.0.1/bin/cellranger count --id=R18072056 \
+--fastqs=/boot2/bixm/02-GuanXu/03_scRNAseq_20210628/Sample_R18072056_0_1_H7KJKDSXX \
+--sample=R18072056 \
+--transcriptome=/boot2/bixm/Software/cellRanger/refdata-gex-GRCh38-2020-A/
+
+/boot2/bixm/Software/cellRanger/cellranger-5.0.1/bin/cellranger count --id=R18072059 \
+--fastqs=/boot2/bixm/02-GuanXu/03_scRNAseq_20210628/Sample_R18072059_0_1_H7KJKDSXX \
+--sample=R18072059 \
+--transcriptome=/boot2/bixm/Software/cellRanger/refdata-gex-GRCh38-2020-A/
+
+/boot2/bixm/Software/cellRanger/cellranger-5.0.1/bin/cellranger count --id=R18072060_ \
+--fastqs=/boot2/bixm/02-GuanXu/03_scRNAseq_20210628/Sample_R18072060_0_1_H7KJKDSXX \
+--sample=R18072060 \
+--transcriptome=/boot2/bixm/Software/cellRanger/refdata-gex-GRCh38-2020-A/
